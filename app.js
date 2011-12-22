8eb07bbf53836177c2c85d00de6ecccf863a90bc
yurotet <yu.rotet@gmail.com>
2011-12-22 19:36:11 +1100

    change main component to be tabpanel

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-
 Ext.regApplication({
 	name : "ES",
 	defaultTarget : "viewport",
 
 	launch : function() {
-		this.viewport = new Ext.Panel({
-			id : "viewport",
-			layout : "fit",
-			fullscreen : "true",
-
-			dockedItems : [ ES.views.TopBar ],
-			items : [ ES.views.PlacesList ]
+		this.viewport = new ES.ViewPort({
+			application:this			
 		});
 	}
 });
 
-
-
-//place model schema
 Ext.regModel('Place', {
 	fields : [ {
 		name : "place_image_url",
@@ -34,7 +25,6 @@ Ext.regModel('Place', {
 		dateFormat : "c"
 	} ]
 });
-
 
 // store model instances locally using localStorage
 Ext.regStore('placesStore', {
@@ -55,93 +45,119 @@ Ext.regStore('placesStore', {
 		place_new_url : 'do ti latere',
 		place_des : 'place1',
 		place_new : true,
-		due_date:new Date(2011,1,3)
+		due_date : new Date(2011, 1, 3)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place2',
 		place_new : false,
-		due_date:new Date(2011,1,5)
+		due_date : new Date(2011, 1, 5)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place3',
 		place_new : false,
-		due_date:new Date(2011,2,3)
+		due_date : new Date(2011, 2, 3)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place4',
 		place_new : false,
-		due_date:new Date(2011,3,5)
+		due_date : new Date(2011, 3, 5)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place5',
 		place_new : true,
-		due_date:new Date(2011,4,3)
+		due_date : new Date(2011, 4, 3)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place6',
 		place_new : false,
-		due_date:new Date(2011,5,3)
+		due_date : new Date(2011, 5, 3)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place7',
 		place_new : false,
-		due_date:new Date(2011,6,3)
+		due_date : new Date(2011, 6, 3)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place8',
 		place_new : false,
-		due_date:new Date(2011,7,3)
+		due_date : new Date(2011, 7, 3)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place9',
 		place_new : false,
-		due_date:new Date(2011,8,3)
+		due_date : new Date(2011, 8, 3)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place10',
 		place_new : false,
-		due_date:new Date(2011,9,3)
+		due_date : new Date(2011, 9, 3)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place11',
 		place_new : false,
-		due_date:new Date(2011,10,3)
+		due_date : new Date(2011, 10, 3)
 	}, {
 		place_new_url : 'do ti latere',
 		place_des : 'place12',
 		place_new : false,
-		due_date:new Date(2011,11,3)
-	}]
-});
-
-
-
-ES.views.PlacesList = new Ext.List({
-	emptyText : '<p class="place-list-empty">没内容，不太可能吧</p>',
-	id : 'placesList',
-	store : 'placesStore',
-	itemTpl : [ '<span class="new_marker"></span>',
-	            '<img src="{place_image_url}"/>',	            
-			'<div class="place-decription">{place_des}</div>"' ]
-});
-
-ES.views.TopBar = new Ext.Toolbar({
-	id : "topbar",
-	layout : "hbot",
-	title : "ES",
-	dock : 'top',
-
-	items : [ {
-		xtype : "spacer"
-	}, {
-		id : 'nav-place',
-		xtype : "button",
-		text : "places"
-	}, {
-		id : "nav-people",
-		xtype : "button",
-		text : "social"
+		due_date : new Date(2011, 11, 3)
 	} ]
-
 });
 
+ES.views.PlacesList = Ext.extend(Ext.List, {
+	id : 'placesList',
+	emptyText : '<p class="place-list-empty">empty content</p>',
+	disableSelection : true,
+	store:'placesStore',
+	
+	plugins : [{
+		ptype : 'listpaging',
+		autoPaging : false
+	}],
+	
+	itemTpl : new Ext.XTemplate([ '<span class="new_marker"></span>',
+			'<img src="{place_image_url}"/>',
+			'<div class="place-decription">{place_des}</div>"' ]),
+	itemCls : 'place-item'
+});
+
+Ext.reg('placesList', ES.views.PlacesList);
+
+ES.ViewPort= Ext.extend(Ext.TabPanel,{
+	id:'viewport',
+	fullscreen:true,
+	ui:'dark',
+	
+	initComponent: function() {
+		Ext.apply(this,{
+			tabBar:{
+				dock:'bottom',
+				layout:{pack:'center'}
+			},
+						
+			cardSwitchAnimation:{
+				type:'slide',
+				cover:true
+			},
+			
+		    items: [{
+		        title: 'Promotions',		        
+		        xtype:'placesList',		        			        
+		        iconCls: 'info',		        
+		        badgeText: '10'
+		    }, {
+		        title: 'Favorites',
+		        html: '<h1>Favorites Places</h1>',
+		        iconCls: 'favorites'
+		    }, 
+		    {
+		        title: 'Friends',
+		        html: '<h1>Weibo activities</h1>',
+		        iconCls: 'user',
+		        badgeText: 'Jay just says:'
+		    }]
+		});
+		
+		ES.ViewPort.superclass.initComponent.apply(this, arguments);
+	}	
+});
