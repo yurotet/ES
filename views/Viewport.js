65ed1882602c7afc51f510cc017c1756dfeeeacc
jiay <jiay@jiay.(none)>
2012-02-03 12:00:53 +1100

    adding search panel

--- views/Viewport.js
+++ views/Viewport.js
@@ -5,38 +5,76 @@ zialer.views.Viewport = Ext.extend(Ext.TabPanel, {
 	ui: 'plain',
 	cardSwitchAnimation: false,
 
-	onAdd : function(cmp, idx) {
+	onAdd: function(cmp, idx) {
 		// intercept the requet of adding spacer component so the spacer
 		// will not be added as the instance of xtype:tab.
-		var cmpToInert = (cmp instanceof Ext.Spacer)? {xtype: 'spacer'}:{
+		var cmpToInsert = (cmp instanceof Ext.Spacer)? {xtype: 'spacer'} : {
 			xtype: 'tab',
 			cls: cmp.tabCls || '',
 			card: cmp
 		};
-		this.tabBar.insert(idx, cmpToInert);
+		this.tabBar.insert(idx, cmpToInsert);
 		this.tabBar.doLayout();
 	},
 
-	initComponent : function() {
+	initComponent: function() {
 		Ext.apply(this, {			
 			items:[				
-				{						
+				{
+					// TODO:: this needs to be replaced with logo image or text.
+					title: (new Date()).getFullYear(),
 					tabCls: 'logo-tab'
 				},
 				{
 					xtype: 'spacer'
 				},
 				{
+					// TODO:: this will be replaced with search icon
+					title: 'serch',
 					tabCls: 'search-tab',
-					xtype: 'panel',
-					ui: 'dark',
+
+					items: [
+						{
+							xtype: 'form',
+							styleHtmlContent: true,
+
+							listeners: {
+								el: {
+									scope: this,
+									submit: this.onQuerySubmit
+								}
+							},
+
+							dockedItems: {
+								xtype: 'toolbar',
+								ui: 'plain',
+								dock: 'top',
+								items: {
+									xtype: 'searchfield',			                    
+				                    name: 'query',
+				                    itemId: 'newQueryField',
+				                    placeHolder: 'zialer zialer...'
+								}
+							}
+						},
+						{ 
+							//TDOO:: implement tiem list panel
+							// xtype: 'itemList'
+						}						
+					]							
 				},
 				{
+					// TDOO:: this will bed replaced with collection icon
+					title: 'coll',
 					tabCls: 'collection-tab'
 				}
 			]
 		});
 
 		zialer.views.Viewport.superclass.initComponent.apply(this,arguments);
-	}
+	},
+
+	onQuerySubmit: function() {
+		console.log('query has submikited');
+	} 
 });
