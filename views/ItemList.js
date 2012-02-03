01d0717cdc1915d1eea3cdcbfb66369b892d29ed
jiay <jiay@jiay.(none)>
2012-02-03 17:13:13 +1100

    item list documentation

--- views/ItemList.js
+++ views/ItemList.js
@@ -1,3 +1,13 @@
+/**
+ * @class zialer.views.ItemList
+ * @extends Ext.list
+ *
+ * The list shows all items retrevied from server based on geo search or collection
+ * search which is sorted by geo distance by default. The body of the item contains
+ * summary and price information for the promoted item, and user is able to swip on 
+ * the item to slide in action panel to do more things around the that item.
+ * 
+ */
 zialer.views.ItemList = Ext.extend(Ext.List, {
 	
 });
